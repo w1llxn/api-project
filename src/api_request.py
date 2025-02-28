45155a845e7f22aa521f7c2f6a511b2957a70b91
w1llxn <art.vsuetnov@gmail.com>
2025-02-28 15:33:18 +0300

    Initial commit: добавлен API-модуль

---
+++ src/api_request.py
@@ -0,0 +1,14 @@
+import requests  
+import json  
+  
+def get_data():  
+    url = "https://api.example.com/data"  
+    response = requests.get(url)  
+    if response.status_code == 200:  
+        return response.json()  
+    else:  
+        return None  
+  
+if __name__ == "__main__":  
+    data = get_data()  
+    print(json.dumps(data, indent=4)) 
